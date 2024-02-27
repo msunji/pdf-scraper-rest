b40fb5b2be202add531f0c58b73a4fec47e60568
msunji <marge.consunji@gmail.com>
2024-02-27 14:46:10 +0800

    Create route for POST requests to get form data

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
+# Create instance of Flask class
 app = Flask(__name__)
 
 @app.route("/")
-
 def hello_world():
     return "<p>Hello!</p>"
+
+@app.route("/api/submit-form", methods=["POST"])
+def submit_form():
+    data = request.json
+
+    pdfUrl = data.get("pdfUrl")
+    reportType = data.get("reportType")
+
+    response = {
+        "message": "Form submitted successfully",
+        "pdfUrl": pdfUrl,
+        "reportType": reportType
+    }
+
+    return jsonify(response), 200
