f4dbbeb948838dc7334daa7a3f01204dfd9b0043
msunji <marge.consunji@gmail.com>
2024-03-04 11:28:27 +0800

    Fix env var load error

--- modules/gsheet_actions.py
+++ modules/gsheet_actions.py
@@ -3,16 +3,16 @@ import os
 
 # GSheets credentials dict
 credentials = {
-  "type": os.getenv("GAPI_TYPE"),
-  "project_id": os.getenv("PROJECT_ID"),
-  "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-  "private_key": os.getenv("PRIVATE_KEY"),
-  "client_email": os.getenv("CLIENT_EMAIL"),
-  "client_id": os.getenv("CLIENT_ID"),
-  "auth_uri": os.getenv("AUTH_URI"),
-  "token_uri": os.getenv("TOKEN_URI"),
-  "auth_provider_x509_cert_url": os.getenv("AUTH_CERT_URL"),
-  "client_x509_cert_url": os.getenv("CLIENT_CERT_URL")
+  "type": os.environ['GAPI_TYPE'],
+  "project_id": os.environ['PROJECT_ID'],
+  "private_key_id": os.environ['PRIVATE_KEY_ID'],
+  "private_key": os.environ['PRIVATE_KEY'].replace(r'\n', '\n'),
+  "client_email": os.environ['CLIENT_EMAIL'],
+  "client_id": os.environ['CLIENT_ID'],
+  "auth_uri": os.environ['AUTH_URI'],
+  "token_uri": os.environ['TOKEN_URI'],
+  "auth_provider_x509_cert_url": os.environ['AUTH_CERT_URL'],
+  "client_x509_cert_url": os.environ['CLIENT_CERT_URL']
 }
 
 all_sheets = gs.service_account_from_dict(credentials)
